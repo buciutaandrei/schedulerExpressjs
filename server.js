293baabcaa9f2bd77f7e4091b64bf7dc02be2ca9
Andrei <buciuta.andrei@gmail.com>
2019-10-31 23:07:39 +0200

    jwt auth

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
-app.use(cors());
+const passport = require("passport");
+const users = require("./api/users");
+const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+const app = express();
 require("dotenv").config();
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
 
 MongoClient.connect(
   "mongodb://localhost/proiect",
@@ -15,17 +27,13 @@ MongoClient.connect(
     db.on("error", error => console.error(error));
     db.once("open", () => console.log("connected to database"));
 
-    app.use(express.json());
+    app.get("/", (req, res) => {});
 
-    app.get("/", (req, res) => {
-      db.collection("asdasd")
-        .find({})
-        .toArray((err, result) => {
-          res.json(result);
-        });
+    app.post("/logging", (req, res) => {
+      console.log(req.body);
     });
 
-    app.get("/:id", (req, res) => {
+    app.get("/database/:id", (req, res) => {
       db.collection(req.params.id)
         .find({})
         .toArray((err, result) => {
@@ -33,20 +41,19 @@ MongoClient.connect(
         });
     });
 
-    app.post("/:id", (req, res) => {
-      let date = req.params.id
+    app.post("/database/:id", (req, res) => {
+      let date = req.params.id;
       db.collection(date)
         .insertOne(req.body)
         .then(data => res.send(data));
     });
 
-    app.patch("/:id", (req, res) => {});
+    app.patch("/database/:id", (req, res) => {});
 
-    app.delete("/:id", (req, res) => {
-
+    app.delete("/database/:id", (req, res) => {
       db.collection(req.params.id)
         .deleteOne({ index: req.body.id })
-        .then(data => res.send(data))
+        .then(data => res.send(data));
     });
 
     app.listen(3001, () => console.log("server started"));
